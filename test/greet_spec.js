a2f204876dac7b4dd802c2feb9edf14432e588af
simsun <sunsj1231@gmail.com>
2015-01-03 03:44:17 +0800

    setting up mocha

---
+++ test/greet_spec.js
@@ -0,0 +1,15 @@
+
+var greet = require('../index')
+describe('greet', function() {
+  it("is a dummy success case", function() {
+    expect(1).to.eql(1);
+  })
+
+  it("should greet a person by name", function() {
+    expect(greet.greet("sim")).to.eql("hello, sim")
+  })
+
+  it("should greet a person flirtatiously if drunk", function() {
+    expect(greet.greet_drink("sim", true)).to.eql("hello sim, you look sexy today")
+  })
+})
